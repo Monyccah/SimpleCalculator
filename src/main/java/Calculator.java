6cad5b4c016ce0bb88c624f3a70698479a7d3f03
Monyccah <moniccahmadingwane@gmail.com>
2020-07-07 14:53:50 +0200

    secondAttempt

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,34 +1,27 @@
-
 public class Calculator {
 
-
     public static void main(String[] args) {
-        int multiSum = addMultiple(1,2,3,4);
-        System.out.println(multiSum);
-        int answer = multiply(10,2);
-        System.out.println(answer);
-        int multianswer= moreMultiple(5,6,7,8,9);
-        int sum = add(1,2);
-        System.out.println(sum);System.out.println(multianswer);
+        System.out.println(add(1,2,3,4,5));
+        System.out.println(add(1,2));
+        System.out.println(add(-1,-1));
+        System.out.println(multiply(1,2,3,4,5));
+        System.out.println(multiply(1,3));
+        System.out.println(multiply(-1,3));
     }
-    public static int add(int a, int b)
-    {        return a + b;    }
-
-    public static int addMultiple(int...x)
-    {        int sum = 0;
-        for (int i = 0; i <x.length ; i++)
-        {            sum += x[i];        }
-        return sum;    }
-
-    public static int multiply(int num1, int num2)
-    {        return num1 * num2;    }
-
-    public static int moreMultiple(double...y)
-    {        int count = 1;
-        for (int i = 0; i < y.length ; i++)
-        {            count *= y[i];        }
-        return count;    }
+    public static int add(int...numbers) {
+        int sum = 0;
+        for(int x = 0; x < numbers.length; x ++)
+        {
+            sum = sum + numbers[x];
+        }
+        return sum;
+    }
+    public static int multiply(int...numbers) {
+        int sum = numbers[0];
+        for(int x = 1; x < numbers.length; x++)
+        {
+            sum = sum * numbers[x];
+        }
+        return sum;
+    }
 }
-
-
-
--- src/test/java/SimpleCalculatorTest.java
+++ src/test/java/SimpleCalculatorTest.java
@@ -1,45 +1,19 @@
 import org.junit.Assert;
 import org.junit.Test;
-
 public class SimpleCalculatorTest {
-
-    public static int add(int a, int b)
-    {        return a + b;    }
+    Calculator calculator = new Calculator();
 
     @Test
     public void addTest() {
-
-        Assert.assertEquals(add(1,5),6);
+        Assert.assertEquals(calculator.add(1, 2, 3, 4, 5), 15);
+        Assert.assertEquals(calculator.add(1, 2), 3);
+        Assert.assertEquals(calculator.add(-1, -1), -2);
     }
 
-    public static int addMultiple(int...x)
-    {        int sum = 0;
-        for (int i = 0; i <x.length ; i++)
-        {            sum += x[i];        }
-        return sum;    }
-
     @Test
-    public void addMultipleTest() {
-
-        Assert.assertEquals(addMultiple(2,3,3),8);
-    }
-
-    public static int multiply(int num1, int num2)   {
-        return num1 * num2;    }
-
-    @Test
-    public void MultiplyTest() {
-
-        Assert.assertEquals(multiply(3,3),9);
-    }
-    public static int moreMultiple(double...y)
-    {        int count = 1;
-        for (int i = 0; i < y.length ; i++)
-        {            count *= y[i];        }
-        return count;    }
-    @Test
-    public void moreMultipleTest() {
-
-        Assert.assertEquals(moreMultiple(2,2,2),8);
+    public void multipleTest() {
+        Assert.assertEquals(calculator.multiply(1, 2, 3, 4, 5), 120);
+        Assert.assertEquals(calculator.multiply(1, 3), 3);
+        Assert.assertEquals(calculator.multiply(-1, 3), -3);
     }
 }
